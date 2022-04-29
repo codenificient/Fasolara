cdeb5557f7e067b41c03709ba58da16aa2982416
CodenificienT <61930093+codenificient@users.noreply.github.com>
2022-04-29 08:08:59 -0400

    darker containers

--- admin/components/Sidebar.js
+++ admin/components/Sidebar.js
@@ -1,6 +1,6 @@
+import Link from 'next/link'
 import styles from '../styles/Sidebar.module.css'
 import Navigation from './Navigation'
-import Link from 'next/link'
 
 export default function Sidebar() {
 	return (
@@ -8,10 +8,10 @@ export default function Sidebar() {
 			<div className={styles.title}>Lara</div>
 			<Navigation />
 			<Link key="13-931-2871" href="/logout" className={styles.Logout}>
-				<span className={styles.Line}>
+				<a className={styles.Line}>
 					<i className="fas fa-sign-out-alt" style={{ color: '#f24e1e', fontSize: '20px' }} />
 					<span className={styles.Name}>Logout</span>
-				</span>
+				</a>
 			</Link>
 		</div>
 	)
