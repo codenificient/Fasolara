539b9c24fced78725657f6bd744f1c95a2f50c36
CodenificienT <61930093+codenificient@users.noreply.github.com>
2022-05-03 22:43:12 -0400

    add search

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -2,13 +2,16 @@ import ActiveProjects from '../components/ActiveProjects'
 import Charts from '../components/Charts'
 import Layout from '../components/Layout'
 import Projects from '../components/Projects'
+import Input from '../components/UI/Input'
 import styles from '../styles/Home.module.css'
 
 function Home() {
 	return (
 		<Layout>
 			<h1 className={styles.title}>Lara admin Dashboard - April 2022</h1>
+
 			<div className="inn_wrapper">
+				<Input placeholder="réchercher les comptes, projets, investisseurs..." label="Recherches"  />
 				<Projects />
 				<Charts />
 				<ActiveProjects />
