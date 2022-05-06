d9c664600611c4d4c05748bbaf8dfe313459ef6b
CodenificienT <61930093+codenificient@users.noreply.github.com>
2022-05-06 15:10:57 -0400

    add edit icon to project

--- admin/components/Project.js
+++ admin/components/Project.js
@@ -1,19 +1,27 @@
-import Member from './Member'
-import Progress from './Progress'
 import Image from 'next/image'
 import styles from '../styles/components/Projects.module.css'
+import Icon from './Icon'
+import Member from './Member'
+import Progress from './Progress'
 
 export default function Project({ number, members, image, progress }) {
-	
 	return (
 		<div className={styles.project_container}>
 			<div className={styles.title}>
-				<h4 className={styles.heading}>Project {number}</h4>
+				<span className={styles.between}>
+					<h4 className={styles.heading}>Project {number}</h4>
+					<Icon classes={'fa-solid fa-pen-to-square'} colors="#FCA320" size={'25px'} />
+				</span>
+
 				<Image className={styles.project_img} src={image} alt={image} width={200} height={45} layout="raw" />
 			</div>
 			<div>
 				<h4 className={styles.heading}>Team Members:</h4>
-				<div className={styles.grid}>{members.map((mbr) => <Member key={mbr.name} name={mbr.name} jobTitle={mbr.jobTitle} imageUrl={mbr.imageUrl} />)}</div>
+				<div className={styles.grid}>
+					{members.map((mbr) => (
+						<Member key={mbr.name} name={mbr.name} jobTitle={mbr.jobTitle} imageUrl={mbr.imageUrl} />
+					))}
+				</div>
 			</div>
 			<div>
 				<h4 className={styles.heading}>Progress:</h4>
